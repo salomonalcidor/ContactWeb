97ec9b74d00fe9c81d98573c6c2e510451c68226
Salomon Alcidor <salomonalcidor@users.noreply.github.com>
2020-01-08 11:57:18 -0500

    Inital Commit

---
+++ ContactWeb/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace ContactWeb.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ContactWeb.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ContactWeb.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
